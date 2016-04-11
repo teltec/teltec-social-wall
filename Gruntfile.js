fe397dd92ce2ecbb1d66faf7851cfb8c7cd38cfd
Jardel Weyrich <jweyrich@gmail.com>
2016-04-11 15:37:33 -0300

    Initial import.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+(function(){
+	"use strict";
+
+	module.exports = function(grunt) {
+
+		grunt.initConfig({
+			jshint: {
+				files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+				options: {
+					globals: {
+						jQuery: true
+					}
+				}
+			},
+			watch: {
+				files: ['<%= jshint.files %>', 'node_modules/', 'bower_components/'],
+				tasks: ['jshint', 'copy']
+			},
+			copy: {
+				main: {
+					files: [
+						{
+							flatten: true,
+							expand: true,
+							src: ['node_modules/moment/min/moment-with-locales.min.js'],
+							dest: 'public/3rd/moment/',
+						},
+						{
+							flatten: true,
+							expand: true,
+							src: [
+								'bower_components/bootstrap/dist/css/*',
+								'bower_components/bootstrap/dist/js/*',
+								'bower_components/bootstrap/dist/fonts/*',
+
+							],
+							dest: 'public/3rd/bootstrap/',
+						},
+						{
+							flatten: true,
+							expand: true,
+							src: [
+								'bower_components/jquery/dist/jquery.min.*',
+								'bower_components/jquery/sizzle/dist/sizzle.min.*',
+							],
+							dest: 'public/3rd/jquery/',
+						},
+					],
+				},
+			}
+		});
+
+		grunt.loadNpmTasks('grunt-contrib-jshint');
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-copy');
+
+		grunt.registerTask('default', ['jshint', 'copy']);
+	};
+})();
---
+++ README.md
@@ -0,0 +1,25 @@
+# Teltec Live Tweets
+
+A web application that shows live tweets that contain a specified hashtag.
+
+## How it works?
+
+It uses a [Node-Red][1] flow that connects with [Twitter][2] to retrieve live tweets filteres by the specified hashtags, then sends the results via [WebSocket][3] to the frontend application.
+
+## How to run?
+
+	git clone https://github.com/teltec/teltec-live-tweets.git
+	cd teltec-live-tweets
+	npm install
+	bower install
+	grunt
+	./bin/www
+
+## TODO
+
+- Promisify dependent events;
+
+
+[1]: http://nodered.org/
+[2]: http://www.twitter.com/
+[3]: http://www.websocket.org/
